b7f3dfa9a994d4f3f17e05f3e40f9709b61e3af5
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-30 15:45:19 +0200

    I am intergratin lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
-function App() {
-  return <div className="text-red-300">Hello</div>;
-}
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { IUrl } from "./interface";
+const App: React.FC = () => {
+  const [url, setUrls] = useState<IUrl[]>([]);
+  const fetchImages = async () => {
+    const request = await axios.get(
+      "https://api.unsplash.com/photos/?client_id=urlARJVsQcnk28YwxFcW9XnviYfx8YKqZtwIk4_9568"
+    );
+    const response = await request.data;
+    setUrls(response);
+  };
+  useEffect(() => {
+    // fetchImages();
+  }, []);
+  useEffect(() => {
+    console.log(url);
+  }, [url]);
+  return (
+    <div className="text-red-300">
+      {url.map((data, index) => {
+        return <img key={index} src={data.urls.raw} />;
+      })}
+    </div>
+  );
+};
 
 export default App;
